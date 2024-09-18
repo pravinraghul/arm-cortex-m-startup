b0edeb7871891820f93f4872d6e7ab60d0bc92d2
pravinraghul <pravinraghul@gmail.com>
2024-09-18 08:22:55 +0530

    Add minimal code docs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## ARM Cortex-M Architecture Startup
 
- - Basic startup code and linker script for ARM Cortex-Mx processors
+ - Basic minimal startup code and linker script for ARM Cortex-Mx processors
  - Compiled and tested basic blinky code.
 
 **Note:** tested in the STM32F429 discovery board.
--- startup.c
+++ startup.c
@@ -5,50 +5,54 @@
 
 /*** SRAM Address Definitions ***/
 #define SRAM_START              0x20000000U
-#define SRAM_SIZE               (192U * 1024U)            // STM32F429xx available SRAM size
+#define SRAM_SIZE               (192U * 1024U) // STM32F429xx available SRAM size
 #define SRAM_END                ((SRAM_START) + (SRAM_SIZE))
 
 /*** Stack Address Definitions ***/
+// Cortex-M is a full descending stack, meaning stack memory starts 
+// from the last address of the memory.
+// Ref: https://developer.arm.com/documentation/dui0552/a/the-cortex-m3-processor/programmers-model/stacks
 #define STACK_START             SRAM_END
 
 /*** Linker Symbols ***/
-extern uint32_t _etext;
-extern uint32_t _sdata;
-extern uint32_t _edata;
-extern uint32_t _sbss;
-extern uint32_t _ebss;
+extern uint32_t end_of_text;
+extern uint32_t start_of_data;
+extern uint32_t end_of_data;
+extern uint32_t start_of_bss;
+extern uint32_t end_of_bss;
 
 int main(void);
 
 /*** Handler Function Prototype ***/
-void Reset_Handler(void);
+void reset_handler(void);
 
 /*** Vector Table Initialization ***/
 // Moving the vector_table array to '.vector_table' section in the code memory.
-uint32_t vector_table[] __attribute__((section(".isr_vector"))) = {
-    (uint32_t) STACK_START,          // Stack Pointer
-    (uint32_t) Reset_Handler,       // Reset Handler
+uint32_t vector_table[] __attribute__((section(".vector_table"))) = {
+    // Basic exceptions required to boot Cortex-M processor
+    (uint32_t) STACK_START,         // Stack Pointer
+    (uint32_t) reset_handler,       // Reset Handler
 };
 
 /*** Handler Function Definitions ***/
-void Reset_Handler(void) {
-    uint8_t *dest = (uint8_t*)&_sdata;
-    uint8_t *src = (uint8_t*)&_etext;
+void reset_handler(void) {
+    uint8_t *dest = (uint8_t*)&start_of_data;
+    uint8_t *src = (uint8_t*)&end_of_text;
     uint32_t size;
-    
+
     // copy .data section to SRAM
-    size = (uint32_t)&_edata - (uint32_t)&_sdata;
+    size = (uint32_t)&end_of_data - (uint32_t)&start_of_data;
     for (uint32_t i = 0; i < size; i++) {
         *dest++ = *src++;
     }
 
-    // Init the .bss section to zero in SRAM
-    size = (uint32_t)&_ebss - (uint32_t)&_sbss;
-    dest = (uint8_t*) &_sbss;
+    // initialize the .bss section to zero in SRAM
+    size = (uint32_t)&end_of_bss - (uint32_t)&start_of_bss;
+    dest = (uint8_t*) &start_of_bss;
     for (uint32_t i = 0; i < size; i++) {
         *dest++=0;
     }
-    
+
     // call the main
     main();
 }
